8eb1124df71f14860de398c2e89991a23e6a8471
AVRozk <avrozk@gmail.com>
2021-02-25 04:51:55 +0300

    add struct SearchResponse update load data

---
+++ MusicApp/SearchResponse.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResponse.swift
+//  MusicApp
+//
+//  Created by andy on 25.02.2021.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    var resultCount: Int
+    var results: [Track]
+}
+
+struct Track: Decodable {
+    var artistName: String
+    var collectionName: String?
+    var trackName: String
+    var artworkUrl100: String?
+}
--- MusicApp/SearchViewController.swift
+++ MusicApp/SearchViewController.swift
@@ -15,10 +15,9 @@ struct TrackModel {
 
 class SearchViewController: UITableViewController {
     
-    let tracks = [
-        TrackModel(trackName: "First track", artistName: "Frist artist"),
-        TrackModel(trackName: "Second track", artistName: "Second artist")
-    ]
+    private var timer: Timer?
+    
+    var tracks: [Track] = []
     
     let searchController = UISearchController(searchResultsController: nil)
     
@@ -67,17 +66,53 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
         
-        let url = "https://itunes.apple.com/search?term=\(searchText)"
-        AF.request(url).responseData { (dataResponse) in
-            if let error = dataResponse.error {
-                print("Error request from: \(url) error: \(error)")
-                return
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { [weak self, searchText] _ in
+            self?.responseData(searchText) { tracks in
+                self?.tracks = tracks
+                self?.tableView.reloadData()
             }
-            
-            guard let data = dataResponse.data else { return }
-            
-            let result = String(data: data, encoding: .utf8)
-            print(result ?? "")
-        }
+        })
+    }
+    
+    func responseData(_ searchText: String, completion: @escaping ([Track]) -> Void) {
+        let url = "https://itunes.apple.com/search"
+        let parameters: [String:Any] = [
+            "term": searchText,
+            "entity": "song",
+            "limit": 10
+        ]
+        print("fetch url: \(url)")
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default)
+//        AF.request(url)
+            .responseDecodable(of: SearchResponse.self) { response in
+                guard let items = response.value else { return }
+                print("OK1")
+                print(items)
+//                completion: @escaping (CachedURLResponse?) -> Void)
+//                self.tracks = items.results
+                completion(items.results)
+          }
+        
+//        AF.request(url).responseData { (dataResponse) in
+//            if let error = dataResponse.error {
+//                print("Error request from: \(url) error: \(error)")
+//                return
+//            }
+//
+//            guard let data = dataResponse.data else { return }
+//
+//            let decoder = JSONDecoder()
+//            do {
+//                let objects = try decoder.decode(SearchResponse.self, from: data)
+//                print("object:", objects)
+//            } catch {
+//                print("Failed to decode JSON", error.localizedDescription)
+//                return
+//            }
+//
+////            let result = String(data: data, encoding: .utf8)
+////            print(result ?? "")
+//        }
     }
 }
