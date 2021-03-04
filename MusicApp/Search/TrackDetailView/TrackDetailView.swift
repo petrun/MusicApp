fe7fbecf5b1cf424cb4c264a97c7d96962b192dd
AVRozk <avrozk@gmail.com>
2021-03-04 08:36:18 +0300

    Custom AudioPlayer

--- MusicApp/Search/TrackDetailView/TrackDetailView.swift
+++ MusicApp/Search/TrackDetailView/TrackDetailView.swift
@@ -18,12 +18,8 @@ class TrackDetailView: UIView {
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var volumeSlider: UISlider!
     
-    let player: AVPlayer = {
-        let player = AVPlayer()
-        player.automaticallyWaitsToMinimizeStalling = false
-        
-        return player
-    }()
+    private var player: AudioPlayerProtocol = AudioPlayer()
+    private let scale: CGFloat = 0.8
     
     override class func awakeFromNib() {
         super.awakeFromNib()
@@ -40,82 +36,24 @@ class TrackDetailView: UIView {
             trackImageView.sd_setImage(with: trackImageUrl)
         }
         
-        let scale: CGFloat = 0.8
         trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
         
-//        monitorStartTime()
-        observePlayerCurrentTime()
-        initPlayer(viewModel.previewUrl)
         volumeSlider.value = player.volume
         currentTimeSlider.value = 0
         
+        player.delegate = self
+        player.addTrack(viewModel.previewUrl)
         player.play()
     }
     
-    private func initPlayer(_ trackUrlString: String?) {
-        guard let trackUrl = URL(string: trackUrlString ?? "") else {
-            return
-        }
-        let trackItem = AVPlayerItem(url: trackUrl)
-        player.replaceCurrentItem(with: trackItem)
-        player.addObserver(self, forKeyPath: "rate", options: [], context: nil)
-    }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "rate", let player = object as? AVPlayer {
-            if player.rate == 1 {
-                print("Playing")
-                enlargeTrackImageView()
-                playPauseButton.setImage(UIImage.init(named: "Pause Button"), for: .normal)
-            } else {
-                print("Paused")
-                reduceTrackImageView()
-                playPauseButton.setImage(UIImage.init(named: "Play Button"), for: .normal)
-            }
-        }
-    }
     
     // MARK: - Time Setup
     
-//    private func monitorStartTime() {
-//        let time = CMTimeMake(value: 1, timescale: 3)
-//        let times = [NSValue(time: time)]
-//        
-//        player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
-//            self?.enlargeTrackImageView()
-//        }
-//    }
-    
-    private func observePlayerCurrentTime() {
-        let interval = CMTimeMake(value: 1, timescale: 2)
-
-        player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] (currentTime) in
-            guard let self = self else { return }
-            self.currentTimeLabel.text = currentTime.toDisplayString()
-            
-            guard let durationTime = self.player.currentItem?.duration else { return }
-            let durationTimeText = (durationTime - currentTime).toDisplayString()
-            
-            self.durationLabel.text = "-\(durationTimeText)"
-            self.updateCurrentTimeSlider()
-        }
-    }
-    
     private func updateCurrentTimeSlider() {
-        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
-        guard let duration = player.currentItem?.duration else { return }
-        let durationSeconds = CMTimeGetSeconds(duration)
+        let currentTimeSeconds = player.currentTimeSeconds
+        guard let durationSeconds = player.durationSeconds else { return }
         let percentage = currentTimeSeconds / durationSeconds
         currentTimeSlider.value = Float(percentage)
-    }
-    
-    private func resetTrack() {
-        let seekTime = CMTimeMakeWithSeconds(0, preferredTimescale: 1)
-        player.seek(to: seekTime)
-    }
-    
-    deinit {
-        print("DEINIT")
     }
     
     // MARK: - Animations
@@ -140,8 +78,7 @@ class TrackDetailView: UIView {
             initialSpringVelocity: 1,
             options: .curveEaseInOut
         ) {
-            let scale: CGFloat = 0.8
-            self.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+            self.trackImageView.transform = CGAffineTransform(scaleX: self.scale, y: self.scale)
         }
     }
     
@@ -152,30 +89,23 @@ class TrackDetailView: UIView {
     }
     
     @IBAction func handleCurrentTimeSlider(_ sender: UISlider) {
+        guard let durationSeconds = player.durationSeconds else { return }
         let percentage = currentTimeSlider.value
-        guard let duration = player.currentItem?.duration else { return }
-        let durationInSeconds = CMTimeGetSeconds(duration)
-        let seekTimeInSeconds = Float64(percentage) * durationInSeconds
-        let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: 1)
-        player.seek(to: seekTime)
+        let seekTimeSeconds = Float64(percentage) * durationSeconds
+        player.seek(to: seekTimeSeconds)
     }
     
     @IBAction func playPauseAction(_ sender: UIButton) {
-        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
-        guard let duration = player.currentItem?.duration else { return }
-        let durationSeconds = CMTimeGetSeconds(duration)
-//        let percentage = currentTimeSeconds / durationSeconds
-//        currentTimeSlider.value = Float(percentage)
-        
+        guard let durationSeconds = player.durationSeconds else { return }
+        let currentTimeSeconds = player.currentTimeSeconds
         
         if currentTimeSeconds == durationSeconds {
-            print("END PLAY")
-            resetTrack()
+            player.resetTrack()
         }
-        if player.timeControlStatus == .paused {
-            player.play()
-        } else {
+        if player.isPlaying {
             player.pause()
+        } else {
+            player.play()
         }
     }
     
@@ -188,6 +118,29 @@ class TrackDetailView: UIView {
     
     @IBAction func handleVolumeSlider(_ sender: UISlider) {
         player.volume = volumeSlider.value
+    }
+    
+}
+
+extension TrackDetailView: AudioPlayerDelegate {
+    func onPlay() {
+        enlargeTrackImageView()
+        playPauseButton.setImage(UIImage.init(named: "Pause Button"), for: .normal)
+    }
+    
+    func onPause() {
+        reduceTrackImageView()
+        playPauseButton.setImage(UIImage.init(named: "Play Button"), for: .normal)
+    }
+    
+    func timeObserver(currentTime: CMTime) {
+        currentTimeLabel.text = currentTime.toDisplayString()
+        
+        guard let durationTime = player.duration else { return }
+        let durationTimeText = (durationTime - currentTime).toDisplayString()
+        
+        durationLabel.text = "-\(durationTimeText)"
+        updateCurrentTimeSlider()
     }
     
 }
---
+++ MusicApp/Services/AudioPlayerService.swift
@@ -0,0 +1,98 @@
+//
+//  AudioPlayerService.swift
+//  MusicApp
+//
+//  Created by andy on 04.03.2021.
+//
+
+import Foundation
+import AVFoundation
+
+protocol AudioPlayerProtocol: class {
+    var isPlaying: Bool { get }
+    var volume: Float { get set }
+    var currentTimeSeconds: Float64 { get }
+    var durationSeconds: Float64? { get }
+    var duration: CMTime? { get }
+    var delegate: AudioPlayerDelegate? { get set }
+    func addTrack(_ trackUrlString: String?)
+    func play()
+    func pause()
+    func seek(to time: Float64)
+    func resetTrack()
+}
+
+class AudioPlayer: AVPlayer, AudioPlayerProtocol {
+    var duration: CMTime? {
+        get {
+            return self.currentItem?.duration
+        }
+    }
+    
+    var isPlaying: Bool {
+        get {
+            return self.rate == 1
+        }
+    }
+    
+    var currentTimeSeconds: Float64 {
+        get {
+            return CMTimeGetSeconds(self.currentTime())
+        }
+    }
+    
+    var durationSeconds: Float64? {
+        get {
+            guard let duration = self.duration else { return nil }
+            return CMTimeGetSeconds(duration)
+        }
+    }
+    
+    weak var delegate: AudioPlayerDelegate?
+    
+    override init() {
+        super.init()
+        
+        self.automaticallyWaitsToMinimizeStalling = false
+        self.addObserver(self, forKeyPath: "rate", options: [], context: nil)
+        
+        let interval = CMTimeMake(value: 1, timescale: 2)
+        self.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] (currentTime) in
+            self?.delegate?.timeObserver(currentTime: currentTime)
+        }
+    }
+    
+    func resetTrack() {
+        self.seek(to: Float64(0))
+    }
+    
+    func seek(to time: Float64) {
+        let seekTime = CMTimeMakeWithSeconds(time, preferredTimescale: 1)
+        self.seek(to: seekTime)
+    }
+    
+    func addTrack(_ trackUrlString: String?) {
+        guard let trackUrl = URL(string: trackUrlString ?? "") else {
+            return
+        }
+        let trackItem = AVPlayerItem(url: trackUrl)
+        self.replaceCurrentItem(with: trackItem)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "rate", let player = object as? AVPlayer {
+            if player.rate == 1 {
+                delegate?.onPlay()
+            } else {
+                delegate?.onPause()
+            }
+        }
+    }
+    
+}
+
+protocol AudioPlayerDelegate: class {
+    func onPlay()
+    func onPause()
+    func timeObserver(currentTime: CMTime)
+}
